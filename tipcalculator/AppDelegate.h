15d36eeee8cef69cba4e6d5888fc0342e89a1a2e
Darlene Wong <darlenew@Darlenes-MacBook-Pro.local>
2015-02-21 16:20:09 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Darlene Wong on 2/21/15.
+//  Copyright (c) 2015 Darlene Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculator
+//
+//  Created by Darlene Wong on 2/21/15.
+//  Copyright (c) 2015 Darlene Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
